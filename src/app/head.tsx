104d4498f3fa46108b89a311016270031a51e72e
adamkovari <kovari.adam@szmacscs.hu>
2023-04-04 20:47:05 +0200

    meta description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Visszajaro</title>
+    <title>Visszajáró - A törzsvendégapplikáció</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta name="Visszajáró - A törzsvendégapplikáció" content="A törzsvendég applikáció: Fogyasztáskövetés, érintésmentes bontott fizetés, személyre szabott kedvezmények, mérhető fogyasztási adatok." />
+    <meta name="description" content="A törzsvendég applikáció: Fogyasztáskövetés, érintésmentes bontott fizetés, személyre szabott kedvezmények, mérhető fogyasztási adatok." />
       <link rel="icon" href="/favicon.ico" />
       <link rel="canonical" href="https://visszajaro.com/" />
     </>
